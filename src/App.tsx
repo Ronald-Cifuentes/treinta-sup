b79281af89793523aaa8db14e83a65d9d215e883
Carlos Equiz <carlos.equiz@treinta.co>
2022-05-20 21:16:00 -0500

    Add App

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable import/no-default-export */
+import {FC} from 'react';
+import {ThemeProvider} from 'styled-components';
+// import {datadogRum} from '@datadog/browser-rum';
+// import {ErrorBoundary} from 'react-error-boundary';
+import {TreintaTheme} from '@30sas/web-ui-kit-theme';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from 'react-query';
+
+// import {Error} from 'modules/Error';
+import {HeadTags} from 'components/molecules';
+import {AuthProvider} from 'context/AuthContext';
+import {DashboardProvider} from 'context/DashboardContext';
+import {ConfigProvider} from 'context/ConfigContext/ConfigContext';
+import {ToastProvider as ToastDashboardProvider} from 'context/ToastContext/ToastContext';
+import {Routes} from './routes';
+import {GlobalStyle} from './config/GlobalStyle';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: Infinity,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+    },
+  },
+});
+const App: FC = () => {
+  // const errorHandler = (error: Error, info): void => {
+  //  // TODO: Add datadog rum
+  //  console.error(error, info);
+  // };
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={TreintaTheme}>
+        <GlobalStyle />
+        <Router>
+          <ConfigProvider>
+            <HeadTags>
+              <AuthProvider>
+                <ToastDashboardProvider>
+                  <DashboardProvider>
+                    <Routes />
+                  </DashboardProvider>
+                </ToastDashboardProvider>
+              </AuthProvider>
+            </HeadTags>
+          </ConfigProvider>
+        </Router>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
