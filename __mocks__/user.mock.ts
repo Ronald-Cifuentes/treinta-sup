d1412788488438e98f880913b13d35bc6f729b44
Carlos Equiz <carlos.equiz@treinta.co>
2022-05-20 21:02:13 -0500

    Create user.mock.ts

---
+++ __mocks__/user.mock.ts
@@ -0,0 +1,40 @@
+export const user = {
+  id: '30626338-3032-5239-a531-333432326562',
+  uid: 'LKkhRyoVyeVQziHjpko603k1Avg2',
+  firstName: 'Carlos Equiz',
+  lastName: '',
+  email: 'carlos.equiz@treinta.co',
+  phone: null,
+  document: '30626338-3032-5239-a531-333432326562',
+  imageUrl:
+    'https://lh3.google.com/u/0/ogw/ADea4I4ovVvP5PeVEIkmqfVJYemXEpBN5VZVYKZJJNRk=s64-c-mo',
+  isOffline: 0,
+  messageToken:
+    'eCS2ZYrHRsq__17Hbv4f0u:APA91bF1Z1D_4LxP20vea-lakknosUiv60UfVFQ8-rrBZQ7o3PTzAkz9C_JwplSYcqll9UEfsOswn4xfS4XK95n0cOO2cK5yllRhiXtLPzE3KvhPOFl0iaTq09rB8-scySeRUm6KcB5p',
+  lastVersion: '1.4.7-2',
+  deviceInfo: null,
+  firstTimes: '{"onboardingWeb":{"inventory":true}}',
+  lastRecommendationAt: null,
+  createdAt: '2022-03-25T21:56:53.000Z',
+  updatedAt: '2022-03-25T21:56:53.000Z',
+  deletedAt: null,
+  documentTypeId: null,
+  userStatusId: 1,
+  countryId: 1,
+  ratingId: '62386235-6332-5335-a134-613063333031',
+  shopRatingId: null,
+  originId: 1,
+  lastSyncedAt: null,
+  region: 'Colombia',
+};
+
+export const userConfig = {
+  uid: 'LKkhRyoVyeVQziHjpko603k1Avg2',
+  email: 'jersson@treinta.co',
+  displayName: 'Jersson Morales',
+  photoURL:
+    'https://lh3.googleusercontent.com/a-/AOh14GjuSVrTx9MO8Bx7btvu9wuxLbcoYundsIr2TXTU=s96-c',
+  phoneNumber: null,
+  authMethod: 1,
+  providerData: [{providerId: 'google'}],
+};
