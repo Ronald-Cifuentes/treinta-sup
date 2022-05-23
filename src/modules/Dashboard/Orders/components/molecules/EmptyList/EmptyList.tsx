2e003ff7a1c8ab4be8655d73b211367ac1012910
Carlos Equiz <carlos.equiz@treinta.co>
2022-05-23 12:23:09 -0500

    Update EmptyList.tsx

--- src/modules/Dashboard/Orders/components/molecules/EmptyList/EmptyList.tsx
+++ src/modules/Dashboard/Orders/components/molecules/EmptyList/EmptyList.tsx
@@ -1,39 +1,20 @@
 import {FC} from 'react';
-import {Trans, useTranslation} from 'react-i18next';
-import {PackageIcon} from '@30sas/web-ui-kit-icons';
-import {Grid, Button} from '@30sas/web-ui-kit-core';
 
-import {ROUTES} from 'routes/types';
-import {useNavigateI18n, useTheme} from 'hooks';
-import {RightBarScreensInventory} from 'config/constants/screens';
-import {useDashboard} from 'context/DashboardContext/DashboardContext';
+import {useTheme} from 'hooks';
 
-import {ButtonStyled} from '../../atoms';
 import {
-  TitleSku,
-  ContainerText,
-  ItemContainer,
   ParentContainer,
-  EmptyTypographyStyled,
 } from './EmptyList.styled';
 import {EmptyListProps} from './types';
+import { Grid } from '@30sas/web-ui-kit-core';
 
 export const EmptyList: FC<EmptyListProps> = ({
-  text,
   height = '40vh',
-  iconSize = '80px',
-  marginX = '24px',
-  hiddenActions = true,
-  isSearchSku = false,
 }) => {
-  const routerNavigate = useNavigateI18n();
-  const {setRightBarOpen, rightBarNavigation} = useDashboard();
-  const {navigate} = rightBarNavigation;
-  const {t} = useTranslation();
   const theme = useTheme();
   return (
     <ParentContainer container style={{height}}>
-      
+      <Grid item xs={12} data-testid="orders_component_emptyList"></Grid>
     </ParentContainer>
   );
 };
