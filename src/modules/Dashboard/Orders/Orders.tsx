3a2796bd556a4d6412b51e3128a949e76ec11ac3
rctreinta <ronald.cifuentes@treinta.co>
2022-06-01 18:27:48 -0500

    feat: add <SpecialTable/> to Orders

--- src/modules/Dashboard/Orders/Orders.tsx
+++ src/modules/Dashboard/Orders/Orders.tsx
@@ -1,74 +1,32 @@
 import {FC} from 'react';
-import {BalanceCard, Divider, VariantsCard} from '@30sas/web-ui-kit-core';
 import {useTranslation} from 'react-i18next';
 import {ColorProps} from '@30sas/web-ui-kit-theme';
 import {DashboardLayout} from 'components/templates';
-import {SuccessPlanIcon} from '@30sas/web-ui-kit-icons';
+import {columns, rowsWithComponents} from '../../../components/atoms/SpecialTable/Mocks';
+import SpecialTable from '../../../components/atoms/SpecialTable';
 
 const LINE_PROPS: ColorProps = {
   baseColor: 'gray',
   gradient: '500',
 };
 
-function createData(
-  id: string,
-  status: string,
-  customer: string,
-  phone: number,
-  createdAt: string,
-  updatedAt: string,
-  deliveryDate: string,
-  value: number
-) {
-  return { id, status, customer, phone, createdAt, updatedAt, deliveryDate, value };
-}
-
-const rows = [
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-  createData('A159845', 'Entregado', 'Carlos Perez', 3015918524, '04/05/2022', '04/05/2022', '04/05/2022', 1500.50),
-];
-
 export const Orders: FC = () => {
-  const { t } = useTranslation();
+  const {t} = useTranslation();
 
   return (
     <DashboardLayout
       title={t('orders.title')}
       fancyLineProps={LINE_PROPS}
       sizeFancyLine="0.5px">
-      <BalanceCard
-        backgroundIconColor="success"
-        backgroundIconColorType="100"
-        icon={SuccessPlanIcon}
-        iconColor="success"
-        iconColorType="400"
-        itemBottom={{
-          label: '500 ventas por cobrar',
-          subLabel: '20 clientes',
-          value: '305000585'
+      <SpecialTable
+        columns={columns}
+        rows={rowsWithComponents}
+        styleContainer={{
+          boxShadow: '2px 4px 8px rgba(34, 38, 42, 0.05)',
+          marginTop: '40px',
+          marginBottom: '56px',
         }}
-        labelColor="success"
-        labelColorType="400"
-        variant={VariantsCard.DEBT}
       />
-      <Divider></Divider>
-      <Divider></Divider>
-      <Divider></Divider>
-
     </DashboardLayout>
   );
 };
