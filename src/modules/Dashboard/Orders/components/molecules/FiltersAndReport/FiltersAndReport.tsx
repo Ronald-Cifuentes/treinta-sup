0aee9ef9609223590bcc28d2c6775b455860625e
rctreinta <ronald.cifuentes@treinta.co>
2022-06-05 19:35:51 -0500

    feat: component <FiltersAndReport/>

---
+++ src/modules/Dashboard/Orders/components/molecules/FiltersAndReport/FiltersAndReport.tsx
@@ -0,0 +1,104 @@
+import React, {FC, useState} from 'react';
+import {Dropdown, TreintaDropdownOptions, Calendar, CalendarRange, CalendarWeek, TreintaDropdownType} from '@30sas/web-ui-kit-core';
+import {optionsTemporalRange} from './Mocks'
+
+export interface PropTypesFiltersAndReport {
+  onChange?:(value: Record<string, Date>) => void
+}
+
+const FiltersAndReport:FC<PropTypesFiltersAndReport> = ({onChange}) => {
+    const [typeOfDate, setTypeOfDate] = useState<string | number | undefined>("Diario")
+  
+    const handleTemporalRange = (data: TreintaDropdownOptions, event: React.ChangeEvent<HTMLInputElement>) =>  {
+      setTypeOfDate(data.value)
+    }
+
+    const handleDate = (value: Record<string, Date>) => {
+      onChange && onChange(value)
+    }
+
+    const changeCalendar = () => {
+      switch (typeOfDate) {
+        case "Diario":
+          return  <Calendar
+                    dataTestId="mockedDateField"
+                    formatDate="d MMM yyyy"
+                    locale="es"
+                    onChange={handleDate}
+                    openTo="day"
+                    required
+                    value={new Date(Date.now())}
+                    views={[ 'day' ]}
+                  />
+        case "Semanal":
+          return  <CalendarWeek
+                    onChange={handleDate}
+                    countCalendar={1}
+                    locale="es"
+                  />
+        case "Mensual":
+          return  <Calendar
+                    dataTestId="mockedDateField"
+                    formatDate="d MMM yyyy"
+                    locale="es"
+                    onChange={handleDate}
+                    openTo="month"
+                    required
+                    value={new Date(Date.now())}
+                    views={[ 'month' ]}
+                  />
+        case "Anual":
+          return  <Calendar
+                    dataTestId="mockedDateField"
+                    formatDate="d MMM yyyy"
+                    locale="es"
+                    onChange={handleDate}
+                    openTo="year"
+                    required
+                    value={new Date(Date.now())}
+                    views={[ 'year' ]}
+                  />
+        case "Rango personalizado":
+          return  <CalendarRange
+                    onChange={handleDate}
+                    countCalendar={1}
+                    locale="es"
+                  />
+      }
+    }
+
+    return <div data-testid='filters-and-report' style={{ display: 'flex', justifyContent: "space-between", alignItems: "center" }}>
+      <div style={{ display: 'flex' }}>
+        <div style={{ width: "162px", height: "40px", marginRight: '8px' }}>
+          <Dropdown
+            AlingMenu="right"
+            dropdownOptions={optionsTemporalRange}
+            elementId="test"
+            errorText="Error text"
+            placeholder={optionsTemporalRange[0].label}
+            defaultValue={optionsTemporalRange[0].label}
+            typeRenderItem={TreintaDropdownType["OnlyLetter"]}
+            onChange={handleTemporalRange} />
+        </div>
+        <div style={{ width: "184px", height: "40px" }}>
+          {changeCalendar()}
+        </div>
+      </div>
+      <div style={{
+        width: "136px",
+        height: "24px",
+        fontFamily: 'Nunito Sans',
+        fontStyle: "normal",
+        fontWeight: "700",
+        fontSize: "16px",
+        lineHeight: "24px",
+        color: "#1A2732",
+        cursor: "pointer"
+      }} onClick={() => alert("Pendiente")}>
+        <i className="fa-light fa-file-arrow-down"></i>
+        Descargar reporte
+      </div>
+    </div>;
+}
+
+export default FiltersAndReport
---
+++ src/modules/Dashboard/Orders/components/molecules/FiltersAndReport/Mocks.ts
@@ -0,0 +1,7 @@
+export const optionsTemporalRange = [
+    { label: 'Diario', value: 'Diario' },
+    { label: 'Semanal', value: 'Semanal' },
+    { label: 'Mensual', value: 'Mensual' },
+    { label: 'Anual', value: 'Anual' },
+    { label: 'Rango personalizado', value: 'Rango personalizado' },
+]
