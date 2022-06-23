e618c6bcdb90ad7cc37fa0fa10faf6ddb51c29f4
Ronald Cifuentes <106139113+rctreinta@users.noreply.github.com>
2022-06-23 10:15:06 -0500

    Update SpecialLineTabs.test.tsx

--- src/components/atoms/SpecialLineTabs/SpecialLineTabs.test.tsx
+++ src/components/atoms/SpecialLineTabs/SpecialLineTabs.test.tsx
@@ -3,7 +3,7 @@ import {SpecialLineTabs} from './SpecialLineTabs';
 
 describe('<SpecialLineTabs/>', () => {
   // UI TESTING
-  test('#1. Exist', () => {
+  test('#1. It should render component', () => {
     render(<SpecialLineTabs />);
     const specialLineTabs = screen.getByTestId('special-line-tabs');
     expect(specialLineTabs).toBeInTheDocument();
