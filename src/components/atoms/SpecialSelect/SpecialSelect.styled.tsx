ea8acac104f66fa331c7988027c5e0f6fce6e00f
rctreinta <ronald.cifuentes@treinta.co>
2022-06-24 22:41:49 -0500

    feat: add component <SpecialSelect/>

---
+++ src/components/atoms/SpecialSelect/SpecialSelect.styled.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const StyledSelect = styled.select`
+  display: flex;
+  width: 100%;
+  height: 40px;
+  padding: 8px 40px 8px 16px;
+  font-size: 16px;
+  line-height: 24px;
+  font-family: 'Nunito Sans', sans-serif;
+  border-radius: 0.5rem;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  background-image: url('data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDUxMiA1MTIiIHdpZHRoPSI1MTIiIGhlaWdodD0iNTEyIj4KCTx0aXRsZT5hcnJvdy1kb3duPC90aXRsZT4KCTxzdHlsZT4KCQkuczAgeyBmaWxsOiAjNjc3MzdlIH0gCgk8L3N0eWxlPgoJPHBhdGggaWQ9IkxheWVyIiBjbGFzcz0iczAiIGQ9Im0xMzIuMyAyMDAuNXEyLjEtNC4yIDguNS02LjQgNi40LTIuMSAxMi44LTIuMSA2LjQgMi4xIDEwLjcgNi40bDcwLjQgNzAuNCAyMS4zIDIxLjMgOTEuNy05MS43cTYuNC02LjQgMTUtNi40IDguNSAwIDE0LjkgNi40IDYuNCA2LjQgNi40IDE0LjkgMCA4LjYtNi40IDE1bC0xMDYuNyAxMDYuNnEtMi4xIDIuMi02LjQgNC4zLTQuMiAyLjEtOC41IDIuMS00LjMgMC04LjUtMi4xLTQuMy0yLjEtNi40LTQuM2wtMTA2LjctMTA2LjZxLTYuNC02LjQtNi40LTE1IDAtNi40IDQuMy0xMi44eiIvPgo8L3N2Zz4=');
+  background-repeat: no-repeat, repeat;
+  background-position: right 0.7em top 50%, 0 0;
+  background-size: 1.5em auto, 100%;
+  background-color: white;
+`;
+
+export const SpecialSelectLabel = styled.p`
+  color: #454d54;
+  margin: 0 0 4px 0;
+  padding: 0;
+  font-family: 'Nunito Sans', sans-serif;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  font-weight: 700;
+  font-style: normal;
+  text-transform: none;
+`;
