4961ffa61bc1736314afbae238fdf3fb0a78a18c
Ronald Cifuentes <106139113+rctreinta@users.noreply.github.com>
2022-11-02 11:32:20 -0500

    :bug: fix: helmet UNSAFE_componentWillMount

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 /* eslint-disable import/no-default-export */
 import {FC, useContext} from 'react';
 import {ThemeProvider} from 'styled-components';
-// import {datadogRum} from '@datadog/browser-rum';
-// import {ErrorBoundary} from 'react-error-boundary';
 import {TreintaTheme} from '@30sas/web-ui-kit-theme';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import {BrowserRouter} from 'react-router-dom';
-
-// import {Error} from 'modules/Error';
-// import {HeadTags} from 'components/molecules';
+import {HeadTags} from 'components/molecules';
 import {AuthProvider} from 'context/AuthContext';
 import {ConfigProvider} from 'context/ConfigContext/ConfigContext';
 import {DashboardProvider} from 'context/DashboardContext';
@@ -28,29 +24,27 @@ const queryClient = new QueryClient({
     },
   },
 });
+
 const App: FC = () => {
   const {loading} = useContext(StoreContext);
-  // const errorHandler = (error: Error, info): void => {
-  //  // TODO: Add datadog rum
-  //  console.error(error, info);
-  // };
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={TreintaTheme}>
         <GlobalStyle />
         <BrowserRouter>
           <ConfigProvider>
-            {/* <HeadTags> */}
-            <AuthProvider>
-              <ToastProvider>
-                <UploadBulkProvider>
-                  <DashboardProvider>
-                    <Routes />
-                  </DashboardProvider>
-                </UploadBulkProvider>
-              </ToastProvider>
-            </AuthProvider>
-            {/* </HeadTags> */}
+            <HeadTags>
+              <AuthProvider>
+                <ToastProvider>
+                  <UploadBulkProvider>
+                    <DashboardProvider>
+                      <Routes />
+                    </DashboardProvider>
+                  </UploadBulkProvider>
+                </ToastProvider>
+              </AuthProvider>
+            </HeadTags>
           </ConfigProvider>
         </BrowserRouter>
         <Backdrop open={loading} />
--- src/components/molecules/HeadTags/HeadTags.tsx
+++ src/components/molecules/HeadTags/HeadTags.tsx
@@ -1,18 +1,19 @@
 import {FC} from 'react';
-import {Helmet} from 'react-helmet';
+// import {Helmet} from 'react-helmet';
+import {Helmet, HelmetProvider} from 'react-helmet-async';
 import {useTranslation} from 'react-i18next';
 
 export const HeadTags: FC = ({children}) => {
   const {t} = useTranslation();
 
   return (
-    <>
+    <HelmetProvider>
       <Helmet>
         <meta name="keywords" content={t('head-tags.keywords')} />
         <meta name="description" content={t('head-tags.description')} />
         <title>{t('head-tags.title')}</title>
       </Helmet>
       {children}
-    </>
+    </HelmetProvider>
   );
 };
